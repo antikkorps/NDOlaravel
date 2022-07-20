f4b76d63b8762849da2749c1c4aa9a47056eccda
Franck Vienot <franckvienot7@gmail.com>
2022-07-21 00:47:09 +0200

    initial commit/init of Auth/Init of Dashboard

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,34 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('Mot de passe oublié? Pas de problème. Inscrivez votre email et nous vous enverrons un lien qui vous permettra de choisir un nouveau mot de passe.') }}
+        </div>
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <div class="block">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-jet-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
